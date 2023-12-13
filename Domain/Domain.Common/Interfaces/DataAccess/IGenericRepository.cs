7d39f54958d0a34cc8bf7bb3fda62f4a787284b0
SrwaFarkha <111921225+SrwaFarkha@users.noreply.github.com>
2023-12-13 14:49:29 +0100

    Add project files.

---
+++ Domain/Domain.Common/Interfaces/DataAccess/IGenericRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace Domain.Common.Interfaces.DataAccess;
+
+public interface IGenericRepository<TEntity, TId> where TEntity : IEntity<TId>
+{
+	Task<TEntity> GetByIdAsync(TId id);
+	Task<IEnumerable<TEntity>> GetAllAsync();
+	Task AddAsync(TEntity entity);
+}
