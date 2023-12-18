85e4dbdd0a924386d632ea478990f9987893b7f5
eriksturen <erik.sturen@iths.se>
2023-12-18 15:14:21 +0100

    docker support, docker compose, dataaccess project, removing https from api.
    running from docker compose up works

--- MicroServices/APIs/Marketing/Marketing.API/Program.cs
+++ MicroServices/APIs/Marketing/Marketing.API/Program.cs
@@ -1,5 +1,24 @@
+using Microsoft.EntityFrameworkCore;
+using Marketing.DataAccess;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var host = Environment.GetEnvironmentVariable("DB_HOST");
+var database = Environment.GetEnvironmentVariable("DB_DATABASE");
+var username = Environment.GetEnvironmentVariable("DB_USER");
+var password = Environment.GetEnvironmentVariable("DB_MSSQL_SA_PASSWORD");
+var connectionString =
+    $"Data Source={host};Initial Catalog={database};User ID={username};Password={password};Trusted_connection=False;TrustServerCertificate=True;";
+
+builder.Services.AddSqlServer<MarketingContext>(connectionString);
+builder.Services.AddScoped<IMarketingRepository, MarketingRepository>();
+
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseHsts();
+}
 
 app.MapGet("/", () => "Hello World!");
 
